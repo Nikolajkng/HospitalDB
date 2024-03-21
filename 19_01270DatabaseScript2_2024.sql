d4a488cffceb28990e9d3592b08c32ccdbcf282b
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-03-21 11:02:15 +0100

    added script task as comment taken from powerpoint

--- 19_01270DatabaseScript2_2024.sql
+++ 19_01270DatabaseScript2_2024.sql
@@ -1,45 +1,21 @@
-Det her er en test
+#########################################################################################################
+# 1) the queries made (as in section 6)
+# 2) the statements used to create and apply functions, procedures, and triggers (as in section 7), and
+# 3) the delete/update statements used to change the tables (as in section 8).
+#########################################################################################################
 
-DROP TABLE Departments
+# Niko
 
-CREATE Table Patients (
-CPR_no  VARCHAR(11) Primary Key,
-Full_name			 VARCHAR(30),
-Address				VARCHAR(30),
-Phone_number		INT(8),
-Email				VARCHAR(20),
-Room				INT(3)
-);
+CREATE TRIGGER NurseID_Before_Insert
+BEFORE INSERT ON Nurses FOR EACH ROW
+BEGIN 
+	SELECT ID as CONCAT('N','Test');
+	SET NEW.Staff_ID = NEW.ID;
+END;
 
-CREATE TABLE Departments (
-Department  VARCHAR(20)PRIMARY KEY,
-Dept_floor INT(2)
-);
 
-CREATE TABLE Doctors (
-Staff_ID VARCHAR(6) PRIMARY KEY,
-Salary INT(9),
-Department VARCHAR(20) REFERENCES Departments(Department),
-Head_of_department BIT(1)
-);
 
-CREATE TABLE Patient_Journals (
-CPR_no 	VARCHAR(11) references Patients(CPR_no),
-Diagnosis	VARCHAR(200),
-DateTime_for_diagnosis	DATETIME,
-Doctor_in_charge_of_diagnosis VARCHAR(6) references Doctors(Staff_ID)
-);
 
-CREATE TABLE DoctorPatients (
-Staff_ID VARCHAR(6) references Doctors(Staff_ID),
-CPR_no VARCHAR(11) references Patients(CPR_no)
-);
-
-CREATE TABLE Nurses (
-Staff_ID VARCHAR(6) PRIMARY KEY,
-Salary INT(6),
-Department VARCHAR(20) references Departments(Department)
-);
 
 
 
