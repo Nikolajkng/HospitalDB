7f8a40e1245dbf99fabcee223e4b559dbdb61b37
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-03-21 11:02:33 +0100

    added comment

--- 19_02170DatabaseScript1_2024.sql
+++ 19_02170DatabaseScript1_2024.sql
@@ -1,4 +1,7 @@
-Det her er en test
+###########################################################################################################
+# 1) the statements used to create the database, its tables and views (as used in section 4 of the report)
+# 2) the statements used to populate the tables (as used in section 5
+###########################################################################################################
 
 DROP TABLE Departments
 
