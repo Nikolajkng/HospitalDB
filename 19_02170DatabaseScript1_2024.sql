c451c46e976e5199c58f3feddd5026f24a9f733f
Stonehench <121813975+Stonehench@users.noreply.github.com>
2024-03-21 11:44:57 +0100

    insert statements in script 1

--- 19_02170DatabaseScript1_2024.sql
+++ 19_02170DatabaseScript1_2024.sql
@@ -3,8 +3,6 @@
 # 2) the statements used to populate the tables (as used in section 5
 ###########################################################################################################
 
-DROP TABLE Departments
-
 CREATE Table Patients (
 CPR_no  VARCHAR(11) Primary Key,
 Full_name			 VARCHAR(30),
@@ -20,7 +18,8 @@ Dept_floor INT(2)
 );
 
 CREATE TABLE Doctors (
-Staff_ID VARCHAR(6) PRIMARY KEY,
+DoctorID INT(6) PRIMARY KEY,
+Name varchar(30),
 Salary INT(9),
 Department VARCHAR(20) REFERENCES Departments(Department),
 Head_of_department BIT(1)
@@ -39,10 +38,52 @@ CPR_no VARCHAR(11) references Patients(CPR_no)
 );
 
 CREATE TABLE Nurses (
-Staff_ID VARCHAR(6) PRIMARY KEY,
+NurseID VARCHAR(6) PRIMARY KEY,
+Name varchar(30),
 Salary INT(6),
 Department VARCHAR(20) references Departments(Department)
 );
 
+INSERT Departments VALUES (
+	'Cardiology', 7),
+	('Sexual Health', 6),
+	('Fertility', -5),
+	('Dermatology', 1),
+	('Breast Screening', 18
+);
 
+# Filler info
+INSERT Doctors Values 
+	('Tobias Martinsen', 694202, 'Sexual Health', 1),
+	('Peter Stensig', 6789096, 'Cardiology'),
+	('Frederik Udby', 324364, 'Fertility'),
+	('Nikolaj Nguyen', 623483, 'Dermatology'),
+	('Christoffer Frost', 80085, 'Breast Screening');
 
+INSERT Nurses values
+	('Kasper Friis', 10000, 'Fertility'),
+	('Margot Robbie', 999999, 'Cardiology'),
+	('Sydney Sweeney', 999998, 'Dermatology'),
+	('Danny DeVito', 420420, 'Sexual Health'),
+	('Christoffers mor', 10500, 'Breast Screening');
+
+INSERT Patients values
+	('260702-3671', 'Christian Vedel Pedersen', 'Nybrogaard kollegie 69a', 30220813, 's224810@student.dtu.dk', 5),
+	('250701-4732', 'Katinka Spangtoft', '', 61341289, 's224805@student.dtu.dk', 9),
+	('240700-7418', 'Marilouise Arbøl', 'Kampsax kollegiet 521', 93990618,'s214401@student.dtu.dk', 10),
+	('130301-3666', 'Michelle Mai', 'NoClue avenue 17', 52690290, 's224771@student.dtu.dk', 4),
+	('300501-7451', 'Thor Skipper', 'Nybrogaard kollegie 69a', 51924474, 's224817@student.dtu.dk', 1)
+	
+INSERT DoctorPatients values
+	('XXXXX', '260702-3671'),
+	('XXXXX', '250701-4732'),
+	('XXXXX', '240700-7418'),
+	('XXXXX', '130301-3666'),
+	('XXXXX', '300501-7451')
+
+INSERT Patient_Journals values
+	('260702-3671', 'Suffers from Bigusdikus', 2002-07-26 02:54:23, 'XXXFredXXX'),
+	('250701-4732', 'Langelandssyndrom stadie 2', 2001-07-25 00:00:00, 'XXXPeterXXX'),
+	('240700-7418', 'Kaps (lethal)', 2022-09-02 08:00:00, 'XXXtobiXXX'),
+	('130301-3666', 'UX fanatic', 2023-06-06, 'XXXNikoXXX'),
+	('300501-7451', 'Suffers from succes', 2022-09-02 08:00:00, 'XXXChristfofferXXX')
